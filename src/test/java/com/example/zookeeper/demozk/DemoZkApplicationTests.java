b6e50f6cb6bb7e298d1beca15320e04d1682874d
aiinspur <aiinspur@outlook.com>
2021-05-06 21:38:01 +0800

    Initial commit

---
+++ src/test/java/com/example/zookeeper/demozk/DemoZkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zookeeper.demozk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoZkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
